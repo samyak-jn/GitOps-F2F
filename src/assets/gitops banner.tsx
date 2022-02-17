71a5f4d8a12526f170d81fb1dddaaf965e4fba90
Samyak Jain <samyak.jn11@gmail.com>
2022-02-17 15:42:07 +0530

    Add final map prototype - 2
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

---
+++ src/assets/gitops banner.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="gitops banner" tilewidth="32" tileheight="32" tilecount="84" columns="21">
+ <image source="../../../../Downloads/imgonline-com-ua-tile-uJZElTyob5S0mvad.jpg" width="699" height="135"/>
+</tileset>
