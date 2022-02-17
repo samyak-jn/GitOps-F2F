c4a25ca91db3ae7936549a740d1e91200d11747d
Samyak Jain <samyak.jn11@gmail.com>
2022-02-17 17:52:52 +0530

    add assets
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

--- src/assets/gitops banner.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="gitopsbanner" tilewidth="32" tileheight="32" tilecount="84" columns="21">
- <image source="openshift-gitops-banner.png" width="699" height="135"/>
-</tileset>
--- src/assets/teambanner.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="teambanner" tilewidth="32" tileheight="32" tilecount="150" columns="15">
- <image source="Gitops_Team.png" width="500" height="350"/>
-</tileset>
