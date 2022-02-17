74a5a8fe83a5cc1df9b7e326c3de6c793762f065
Samyak Jain <samyak.jn11@gmail.com>
2022-02-17 15:55:59 +0530

    Add final map prototype - 2
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

---
+++ src/assets/teambanner.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="teambanner" tilewidth="32" tileheight="32" tilecount="150" columns="15">
+ <image source="../../../../Downloads/Gitops Team - 2021-22.png" width="500" height="350"/>
+</tileset>
